0b16cf418bcf18b797037273b20264c1fc314d3a
Jasper Arneberg <C16Jasper.Arneberg@usafa.edu>
2014-04-07 21:56:33 -0600

    simplified ALU code

--- ALU_shell.vhd
+++ ALU_shell.vhd
@@ -44,7 +44,6 @@ architecture ALU of ALU is
 signal Temp : STD_LOGIC_VECTOR(3 downto 0);
 
 begin
-	
 -- fill in details to create result as a function of Data and Accumulator, based on OpSel.
  -- e.g : Build a multiplexer choosing between the eight ALU operations.  Either use a case statement (and thus a process)
  --       or a conditional signal assignment statement ( x <= Y when <condition> else . . .)
@@ -59,36 +58,22 @@ begin
 --  6     : ADD
 --  7     : LD
 aluswitch: process (Accumulator, Data, OpSel)
-        begin
-			Temp(0) <= not Accumulator(0);
-			Temp(1) <= not Accumulator(1);
-			Temp(2) <= not Accumulator(2);
-			Temp(3) <= not Accumulator(3);
-		  
+        begin 
 		-- enter your if/then/else or case statements here
 			case Opsel is 
 				when "000" => --AND
-					Result(0) <= Data(0) and Accumulator(0);
-					Result(1) <= Data(1) and Accumulator(1);
-					Result(2) <= Data(2) and Accumulator(2);
-					Result(3) <= Data(3) and Accumulator(3);
+					Result <= Accumulator and Data;
 				when "001" => --NEG (2's complement)
-					Result <= Temp + "0001";
+					Result <= not Accumulator + "0001";
 				when "010" => --NOT (invert)
-					Result(0) <= not Accumulator(0);
-					Result(1) <= not Accumulator(1);
-					Result(2) <= not Accumulator(2);
-					Result(3) <= not Accumulator(3);
+					Result <= not Accumulator;
 				when "011" => --ROR
 					Result(0) <= Accumulator(1);
 					Result(1) <= Accumulator(2);
 					Result(2) <= Accumulator(3);
 					Result(3) <= Accumulator(0);
 				when "100" => --OR
-					Result(0) <= Accumulator(0) or Data(0);
-					Result(1) <= Accumulator(1) or Data(1);
-					Result(2) <= Accumulator(2) or Data(2);
-					Result(3) <= Accumulator(3) or Data(3);
+					Result <= Accumulator or Data;
 				when "101" => --IN
 					Result <= Data;
 				when "110" => --ADD
@@ -99,8 +84,5 @@ aluswitch: process (Accumulator, Data, OpSel)
 					Result <= "XXXX";
 			end case;
 		end process;
-
--- OR, enter your conditional signal statement here
-
 end ALU;
 
